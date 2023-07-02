6514fce92a14fe99364b2447126efe7892a5ee3a
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2023-07-02 15:51:17 -0300

    Update labirinto.c

--- labirinto.c
+++ labirinto.c
@@ -119,6 +119,21 @@ void posMause(Labirinto* pLab, Posicao* mause){
     }
 }
 
+
+int corrigiLab(Labirinto* pLab){
+
+    for (int i = 0; i < pLab->tamL; i++){
+        for (int j = 0; j < pLab->tamC; j++){
+            if (pLab->mapa[i][j] == 'b')
+            {
+                pLab->mapa[i][j] =' ';
+            }
+        }
+    }
+
+    return 1;
+}
+
 int achaSaida(Labirinto* pLab, Posicao *saida, Posicao *mause, Percurso *pTra, int i,Posicao *inicio, int achou){
     //função recursiva
     
@@ -187,11 +202,122 @@ int achaSaida(Labirinto* pLab, Posicao *saida, Posicao *mause, Percurso *pTra, i
     return achou;
 }
 
+
+//função que acha a saida utilizando pilha e metodo de procura em profundidade
+int achaSaidaPilha(Labirinto* pLab, Posicao *saida, Posicao *mause, Percurso *pTra, int i, int achou){
+    Pilha *pPilha;
+    Pilha *pPilhaCaminho;
+    pPilha = PilhaInicia(verificavazio(pLab));
+    pPilhaCaminho = PilhaInicia(verificavazio(pLab));
+    
+    Item item;
+   
+    while(mause->x != saida->x || mause->y != saida->y){
+       
+        pLab->mapa[valueY(mause)][valueX(mause)] = 'b';
+        
+        item.x = mause->x;
+        item.y = mause->y;
+        item.pai.x = mause->x;
+        item.pai.y = mause->y;
+        
+      if (valueY(mause) - 1 > 0 && valueY(mause) - 1 < pLab->tamL && pLab->mapa[valueY(mause) - 1][valueX(mause)] == ' '){
+            //teste para Cima
+            item.y--;
+            insertPilha(pPilha, item);
+            item.y++;
+        }
+
+        if (valueX(mause) - 1 > 0 && valueX(mause) - 1 < pLab->tamC && pLab->mapa[valueY(mause)][valueX(mause) - 1] == ' '){   
+            //teste para esquerda
+            item.x--;
+            insertPilha(pPilha, item);
+            item.x++;
+            
+        }
+
+  
+        
+        if (valueY(mause) + 1 > 0 && valueY(mause) + 1 < pLab->tamL && pLab->mapa[valueY(mause) + 1][valueX(mause)] == ' '){
+            //teste para baixo
+            item.y++;
+            insertPilha(pPilha, item);
+            item.y--;
+            
+        }
+
+
+        if (valueX(mause) + 1 > 0 && valueX(mause) + 1 < pLab->tamC && pLab->mapa[valueY(mause)][valueX(mause) + 1] == ' '){
+            //teste para direita
+            item.x++;
+            insertPilha(pPilha, item);
+            item.x--;
+            
+        }
+        
+        //atualiza o valor do rato com o ultimo valor dapilha principal pPilha
+        updatePos(mause, pPilha->item[pPilha->ultimo - 1].y, pPilha->item[pPilha->ultimo - 1].x);
+        
+      
+        //Enquanto o item o ultimo da Pilha caminho não for o pai do item que tera a proxima posição do mause, vai remover itens
+      
+        while( !testePaiItem(pPilhaCaminho->item[pPilhaCaminho->ultimo - 1], pPilha->item[pPilha->ultimo - 1]) ){
+            if(PilhaEhVazia(pPilhaCaminho)){
+                break;
+            }
+            
+            removePilha(pPilhaCaminho);
+
+        }
+        
+        //inseri na pilha pPilhaCaminho a as coordenadas co caminho endo feito
+        insertPilha(pPilhaCaminho, pPilha->item[pPilha->ultimo - 1]);
+        
+
+        if(mause->x == saida->x && mause->y == saida->y){
+            //printf("\nachou a saida\n");
+            achou = 1;
+            break;
+            }
+        else if(PilhaEhVazia(pPilha) && achou == 0){
+            printf("\nultimo %i\n",pPilha->ultimo);
+            printf("\n\nnão achou saida\n\n");
+            printf("\n\n%i\n\n",PilhaEhVazia(pPilha));
+            desalocaPilha(&pPilhaCaminho);
+            desalocaPilha(&pPilha);
+            return achou;
+            break;
+
+        }
+        removePilha(pPilha);
+
+    }
+    
+    //retira os caracteres b do labirinto, o caracter b é utilizado para o rato não passa em uma posição mais de uma vez
+    corrigiLab(pLab);
+
+    //passa os valores da pilha caminho para o vetor trajeto
+    while(i < pPilhaCaminho->ultimo){
+
+        updateTra(pTra, i,  pPilhaCaminho->item[i].x,pPilhaCaminho->item[i].y);
+        pTra->mCaminho[i] = pTra->trajetos[i];
+        i++;
+    }
+
+    pTra->mcom = i;
+
+    //desaloca as pilhas utilizadas
+    desalocaPilha(&pPilhaCaminho);
+    desalocaPilha(&pPilha);
+    return 1;
+}
+
+
 Labirinto* imprimepercursoNolabirinto(Labirinto *plab,Percurso *pTra){
     //impressão da saida de acordo com a opção de entrada
     
     //imprime o menor percurso feito
-    if(plab->op == 'p' || plab->op == 'p'){
+    if(plab->op == 'p' || plab->op == 'r' || plab->op == 'f'){
         
         for (int i = 0; i < pTra->mcom; i++){
             Posicao pos = pTra->mCaminho[i];
@@ -215,3 +341,4 @@ Labirinto* imprimepercursoNolabirinto(Labirinto *plab,Percurso *pTra){
     
     return plab;
 }
+
